17c5dabc8bd0663cc7f036af4e22dbaf983ebfdd
hamzaA1ee <hamzaa.zenkoders@gmail.com>
2024-09-17 03:29:53 -0700

    unitialized the schema

--- src/schema/schedule.schema.ts
+++ src/schema/schedule.schema.ts
@@ -1,10 +1,10 @@
-import { object, string, number, date, InferType } from 'yup';
+// import { object, string, number, date, InferType } from 'yup';
 
-const scheduleSchema = object({
-  userId: string().required('user is required'),
-  date: date().required('date is required'),
-  scheduleStart: date().required('schedule start date is required'),
-  scheduleEnd: date().required('schedule end date is required'),
-  site: string().required('site is required'),
-  job: string().required('job is required'),
-});
+// const scheduleSchema = object({
+//   userId: string().required('user is required'),
+//   date: date().required('date is required'),
+//   scheduleStart: date().required('schedule start date is required'),
+//   scheduleEnd: date().required('schedule end date is required'),
+//   site: string().required('site is required'),
+//   job: string().required('job is required'),
+// });
